b0a2b963a52240472f84f1b1df09dbd40aea9dd7
Audrey Tang <audreyt@audreyt.org>
2013-01-30 20:22:53 +0800

    * Another 9 chars used in non-def fields

--- db2unicode.pl
+++ db2unicode.pl
@@ -1,5 +1,9 @@
 #!/usr/bin/env perl
 # 用法：perl db2unicode.pl | sqlite3 development.unicode.sqlite3
+#
+# 如欲使用 fontforge/HANNOMMoEExtra-Regular 的 PUA 字形，請改用：
+# perl db2unicode.pl sym-pua.txt | sqlite3 development.pua.sqlite3
+#
 use strict;
 use utf8;
 use encoding 'utf8';
@@ -7,7 +11,7 @@ use FindBin '$Bin';
 die "development.sqlite3 not in current directory!" unless -s "development.sqlite3";
 my %map = do {
     local $/;
-    open my $fh, '<:utf8', "$Bin/sym.txt";
+    open my $fh, '<:utf8', (@ARGV ? $ARGV[0] : "$Bin/sym.txt") or die $!;
     map { split(/\s+/, $_, 2) } split(/\n/, <$fh>);
 };
 my $re = join '|', keys %map;
