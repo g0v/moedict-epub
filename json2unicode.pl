7122f4c571e4b8b63a326a55e6c09cfa10e55dce
Audrey Tang <audreyt@audreyt.org>
2013-02-05 01:43:02 +0800

    * json2unicode.pl updated for dict-revised.json

--- json2unicode.pl
+++ json2unicode.pl
@@ -1,14 +1,14 @@
 #!/usr/bin/env perl
-# 用法：perl json2unicode.pl > output.unicode.json
+# 用法：perl json2unicode.pl > dict-revised.unicode.json
 #
 # 如欲使用 fontforge/HANNOMMoEExtra-Regular 的 PUA 字形，請改用：
-# perl json2unicode.pl sym-pua.txt > output.unicode.json
+# perl json2unicode.pl sym-pua.txt > dict-revised.pua.json
 #
 use strict;
 use utf8;
 use encoding 'utf8';
 use FindBin '$Bin';
-die "output.json not in current directory!" unless -s "output.json";
+die "dict-revised.json not in current directory!" unless -s "dict-revised.json";
 my %map = do {
     local $/;
     open my $fh, '<:utf8', (@ARGV ? $ARGV[0] : "$Bin/sym.txt") or die $!;
@@ -16,7 +16,7 @@ my %map = do {
 };
 my $re = join '|', keys %map;
 my $compat = join '|', map { substr($_, 1) } grep /^x/, keys %map;
-open my $dump, '<:utf8', 'output.json';
+open my $dump, '<:utf8', 'dict-revised.json';
 my $seen_fcf2 = 0;
 local $/;
 while (<$dump>) {
