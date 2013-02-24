c177798473a3dd982f249de0d4ab0a85900019ad
Kang-min Liu <gugod@gugod.org>
2013-02-24 10:54:16 +0100

    read data from json

--- to-epub.pl
+++ to-epub.pl
@@ -1,18 +1,6 @@
 #!/usr/bin/ev perl
 use v5.14;
 use strict;
-
-BEGIN {
-    die <<USAGE unless -e "dict-revised.unicode.sqlite3";
-
-File missing. Run this first:
-
-   perl db2unicode.pl | sqlite3 dict-revised.unicode.sqlite3
-
-
-USAGE
-}
-
 use utf8;
 use encoding 'utf8';
 use EBook::EPUB;
@@ -23,7 +11,9 @@ use Unicode::Properties qw(uniprops);
 use Unicode::Collate;
 use Unicode::Collate::CJK::Stroke;
 use DBI;
+use JSON;
 use Mojo::Template;
+use List::MoreUtils qw(uniq);
 
 sub mt { state $mt = Mojo::Template->new }
 
@@ -32,8 +22,16 @@ sub sort_by_cjk_strokes {
     return $sorter->sort(@_);
 }
 
-sub dbh_moedict() {
-    state $dbh = DBI->connect("dbi:SQLite:dbname=dict-revised.unicode.sqlite3", "", "", { sqlite_unicode => 1 });
+sub load_whole_dict {
+    state $dict = JSON::decode_json(read_file("../moedict-data/dict-revised.json"));
+    state $chars_with_radical = do {
+        my $x = {};
+        for my $char (@$dict) {
+            push @{ $x->{$char->{radical}} ||=[]}, $char;
+        }
+        $x;
+    };
+    return ($dict, $chars_with_radical);
 }
 
 sub radicals {
@@ -50,90 +48,29 @@ sub radicals {
     # ⼅
     # ⼛
 
-    @radicals = @{ dbh_moedict->selectcol_arrayref("SELECT DISTINCT radical FROM entries WHERE length(title) = 1 AND radical IS NOT NULL") };
-
-    if (grep { !$_ } @radicals) {
-        warn "Warning: the list of radicals contains falsy value\n";
-    }
-
+    my ($dict, undef) = load_whole_dict();
+    @radicals = uniq grep { $_ } map { $_->{radical} } @$dict;
     @radicals = sort_by_cjk_strokes @radicals;
     return @radicals;
 }
 
 sub chars_with_radical {
     my $radical = shift;
-    my $chars = dbh_moedict->selectcol_arrayref("SELECT title FROM entries WHERE length(title) = 1 AND radical = ? ORDER by stroke_count ASC", {}, $radical);
-
-    return @$chars;
+    my ($dict, $chars_with_radical) = load_whole_dict();
+    return $chars_with_radical->{$radical};
 }
-
-sub definitions_of_heteronym {
-    my $heteronym_id = shift;
-    my $def = dbh_moedict->selectall_arrayref("SELECT type, def FROM definitions WHERE heteronym_id = ?", { Slice => {} }, $heteronym_id);
-    return $def;
-}
-
-sub heteronyms_of_character {
-    my $character = shift;
-    my ($entry_id) = @{dbh_moedict->selectcol_arrayref("SELECT id FROM entries WHERE title = ?", {}, $character)};
-    my $heteronyms = dbh_moedict->selectall_arrayref("SELECT id,bopomofo,bopomofo2,pinyin FROM heteronyms WHERE entry_id = ?", { Slice => {} }, $entry_id);
-
-    for (@$heteronyms) {
-        $_->{definitions} = definitions_of_heteronym( $_->{id} );
-    }
-
-    return $heteronyms
-}
-
-sub template_chapter() {
-#   <style>body { margin-left: 5%; margin-right: 5%; margin-top: 5%; margin-bottom: 5%; text-align: justify; -epub-writing-mode: vertical-rl;}</style>
-
-    return <<'TEMPLATE';
-% my ($title, $content) = @_;
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-  <title><%= $title %></title>
-</head>
-<body>
-  <h1><%= $title %></h1>
-  <%= $content %>
-</body>
-</html>
-TEMPLATE
-}
-
-sub template_character {
-    return <<'TEMPLATE';
-% my ($character, $heteronyms) = @_;
-% for my $h (@$heteronyms) {
-<h3><%= $character %><%= $h->{bopomofo} if $h->{bopomofo} %></h3>
-%   for my $d (@{$h->{definitions}}) {
-<p>
-% if ($d->{type}) {
-<strong><%= $d->{type} %></strong>
-% }
-<%= $d->{def} %>
-</p>
-%   }
-% }
-TEMPLATE
-}
-
 
 sub build_chapter {
-    my ($radical, @characters) = @_;
+    my ($radical, $characters) = @_;
     my $title = "$radical 部";
     my $content = "";
 
-    for my $c (@characters) {
-        my $h = heteronyms_of_character($c);
-        $content .= mt->render(template_character, $c, $h);
+    for my $c (@$characters) {
+        my $h = $c->{heteronyms};
+        $content .= mt->render_file("views/character.html.ep", $c->{title}, $h);
     }
 
-    $content = mt->render(template_chapter, $title, $content);
+    $content = mt->render_file("views/chapter.html.ep", $title, $content);
 
     return {
         title => $title,
