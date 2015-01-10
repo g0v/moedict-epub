25db9da3fa41eba8bc2a821836d04c5b7b63272f
Kang-min Liu <gugod@gugod.org>
2015-01-11 00:43:34 +0100

    add doc frequency term position.

--- build-pinyin-index.pl
+++ build-pinyin-index.pl
@@ -5,6 +5,36 @@ use JSON;
 use File::Slurp;
 use Unicode::Normalize;
 
+sub insert_index {
+    my ($idx, $title, $terms) = @_;
+
+    my (%pos, %freq);
+    for (my $i = 0; $i < @$terms; $i++) {
+        my $t = $terms->[$i];
+        $freq{$t}++;
+        push @{$pos{$t}},$i;
+    }
+    for my $t (keys %freq) {
+        $idx->{$t}{$title} //= [$pos{$t}[0], 0];
+        $idx->{$t}{$title}[1] += $freq{$t};
+    }
+}
+
+sub sort_index {
+    my $idx = shift;
+    reset(%$idx);
+    while (my ($term, $docs) = each %$idx) {
+        my @rows = map {
+            [ $_, $docs->{$_}[0], $docs->{$_}[1] ]
+        } sort {
+            length($a) <=> length($b)
+            || $docs->{$a}[0] <=> $docs->{$b}[0]
+            || $docs->{$b}[1] <=> $docs->{$a}[1]
+        } keys %$docs;
+        $idx->{$term} = \@rows;
+    }
+}
+
 binmode STDERR, ":utf8";
 
 my $dict = from_json(scalar read_file "dict-revised-unicode.json", { binmode => ":utf8" });
@@ -16,12 +46,12 @@ my %pinyin_sans_tone;
 my %tones = ( "\x{304}" => 1 , "\x{301}" => 2, "\x{30c}" => 3 , "\x{300}" => 4 );
 my $tone_re = "(" . join("|", keys %tones) . ")";
 
-for (my $i = 0; $i < $#$dict; $i++) {
+for (my $i = 0; $i < @$dict; $i++) {
     my $entry = $dict->[$i];
     for my $heteronym (@{ $entry->{heteronyms} }) {
         next unless $heteronym->{pinyin};
 
-        for my $p (split /\(變\)/, $heteronym->{pinyin}) {
+        for my $p (split /\s*\(變\)\s*/, $heteronym->{pinyin}) {
             # strip comments like:（讀音）(又音)(語音)
             $p =~ s/（.音）//g;
             $p =~ s/\(.音\)//g;
@@ -35,13 +65,18 @@ for (my $i = 0; $i < $#$dict; $i++) {
             if ($p1 !~ /\A[ a-z1234]+\z/) {
                 say STDERR "This looks weird: $p";
             } else {
-                push @{ $pinyin{$p} }, $entry->{title};
-                push @{ $pinyin_sans_tone{$p0} }, $entry->{title};
-                push @{ $pinyin_numerical_tone{$p1} }, $entry->{title};
+                my $title = $entry->{title};
+                insert_index(\%pinyin, $title, [split /\s+/, $p]);
+                insert_index(\%pinyin_sans_tone, $title, [split /\s+/, $p0]);
+                insert_index(\%pinyin_numerical_tone, $title, [split /\s+/, $1]);
             }
         }
     }
 }
+
+sort_index(\%pinyin);
+sort_index(\%pinyin_sans_tone);
+sort_index(\%pinyin_numerical_tone);
 
 my $JSON = JSON->new->pretty->utf8->canonical;
 write_file "data/index-pinyin.json", $JSON->encode(\%pinyin);
